9b58f9d21c1805c1cb8a7cfead33a1b1c8b377f0
ryan961 <ryan961azy@gmail.com>
2024-07-02 16:11:40 +0800

    Update DefaultConverter.

--- converter.go
+++ converter.go
@@ -1,18 +1,14 @@
 package slogopenobserve
 
 import (
-	"net/http"
-
 	"log/slog"
+	"maps"
 
 	slogcommon "github.com/samber/slog-common"
 )
 
 var SourceKey = "source"
-var ContextKey = "extra"
 var ErrorKeys = []string{"error", "err"}
-var RequestKey = "request"
-var RequestIgnoreHeaders = false
 
 type Converter func(addSource bool, replaceAttr func(groups []string, a slog.Attr) slog.Attr, loggerAttr []slog.Attr, groups []string, record *slog.Record) map[string]any
 
@@ -21,46 +17,33 @@ func DefaultConverter(addSource bool, replaceAttr func(groups []string, a slog.A
 	attrs := slogcommon.AppendRecordAttrsToAttrs(loggerAttr, groups, record)
 
 	// developer formatters
-	attrs = slogcommon.ReplaceError(attrs, ErrorKeys...)
 	if addSource {
 		attrs = append(attrs, slogcommon.Source(SourceKey, record))
 	}
 	attrs = slogcommon.ReplaceAttrs(replaceAttr, []string{}, attrs...)
+	attrs = slogcommon.RemoveEmptyAttrs(attrs)
 
 	// handler formatter
-	extra := slogcommon.AttrsToMap(attrs...)
-
-	payload := map[string]any{
+	log := map[string]any{
+		"@timestamp":     record.Time.UTC(),
 		"logger.name":    name,
 		"logger.version": version,
-		"timestamp":      record.Time.UTC(),
 		"level":          record.Level.String(),
 		"message":        record.Message,
 	}
 
+	extra := slogcommon.AttrsToMap(attrs...)
+
 	for _, errorKey := range ErrorKeys {
-		if v, ok := extra[errorKey]; ok {
-			if err, ok := v.(error); ok {
-				payload[errorKey] = slogcommon.FormatError(err)
+		if err, ok := extra[errorKey]; ok {
+			if e, ok := err.(error); ok {
+				log[errorKey] = slogcommon.FormatError(e)
 				delete(extra, errorKey)
 				break
 			}
 		}
 	}
 
-	if v, ok := extra[RequestKey]; ok {
-		if req, ok := v.(*http.Request); ok {
-			payload[RequestKey] = slogcommon.FormatRequest(req, RequestIgnoreHeaders)
-			delete(extra, RequestKey)
-		}
-	}
-
-	if user, ok := extra["user"]; ok {
-		payload["user"] = user
-		delete(extra, "user")
-	}
-
-	payload[ContextKey] = extra
-
-	return payload
+	maps.Copy(log, extra)
+	return log
 }
